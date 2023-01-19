8b16d58aee04761c3bdb37e6917d7d1974d02c7d
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-01-19 19:31:42 -0300

    add middlewares

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,11 +2,16 @@ import { Router } from "express";
 
 import { CreateUserController } from "./controllers/User/CreateUserController";
 import { AuthUserController } from "./controllers/User/AuthUserController";
+import { DetailUserController } from "./controllers/User/DetailUserController";
+
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 // rotas user
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+
+router.get("/me", isAuthenticated, new DetailUserController().handle);
 
 export { router };
---
+++ backend/src/services/user/DetailUserService.ts
@@ -0,0 +1,9 @@
+import prismaClient from "../../prisma";
+
+class DetailUserService {
+  async execute() {
+    return true;
+  }
+}
+
+export { DetailUserService };
