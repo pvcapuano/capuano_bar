48d9586ae83c3e435d3b5cd3ca28513414e71fb8
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-01-30 19:43:40 -0300

    finished login screen

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link rel="icon" href="/beer.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import logoImg from "../../public/beer.svg";
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
@@ -11,13 +10,19 @@ export default function Home() {
         <title>Capuano Bar | Login</title>
       </Head>
       <div className={styles.containerCenter}>
-        <h1>Capuano Bar</h1>
+        <h1 className={styles.title}>Capuano Bar</h1>
 
         <div className={styles.login}>
           <form>
             <Input placeholder="Email" type="text" />
             <Input placeholder="Senha" type="password" />
+
+            <Button type="submit" loading={false}>
+              Entrar
+            </Button>
           </form>
+
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
         </div>
       </div>
     </>
