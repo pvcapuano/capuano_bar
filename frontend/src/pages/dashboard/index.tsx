783f723739fedf99c33983bed452677f8cea5a8f
Paulo Victor Capuano <pvcapuano@gmail.com>
2023-02-08 19:48:11 -0300

    find orders

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,20 +1,67 @@
 import Head from "next/head";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Header } from "../../components/Header";
+import styles from "./styles.module.scss";
+import { FiRefreshCcw } from "react-icons/fi";
+import { setupAPIClient } from "../../services/api";
+import { useState } from "react";
 
-export default function Dashboard() {
+type OrderProps = {
+  id: string;
+  table: string | number;
+  status: boolean;
+  draft: boolean;
+  name: string | null;
+};
+interface HomeProps {
+  orders: OrderProps[];
+}
+
+export default function Dashboard({ orders }: HomeProps) {
+  const [orderList, setOrderList] = useState(orders || []);
+
+  function handleOpenModalView(id: string) {
+    alert("teste");
+  }
   return (
     <>
       <Head>
         <title>Capuano Bar | Painel </title>
       </Head>
-      <Header></Header>
+      <Header />
+      <div>
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Ultimos pedidos</h1>
+            <button>
+              <FiRefreshCcw size={25} color="#fff" />
+            </button>
+          </div>
+
+          <article className={styles.listOrders}>
+            {orderList.map((item) => (
+              <section key={item.id} className={styles.orderItem}>
+                <button onClick={() => handleOpenModalView(item.id)}>
+                  <span>{item.table}</span>
+                </button>
+              </section>
+            ))}
+          </article>
+        </main>
+      </div>
     </>
   );
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get("/orders");
+
+  console.log(response.data);
   return {
-    props: {},
+    props: {
+      orders: response.data,
+    },
   };
 });
